e3b1b9538cba44a57ec39a02f6a2de1911ed095b
Ranjan Sharma <ranjansharma255@gmail.com>
2021-01-30 20:54:10 +0530

    Updated My own Traffic Signal Program

--- TrafficLight.java
+++ TrafficLight.java
@@ -1,72 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package MiniProject_01_TrafficLight_Simulation;
-
-/**
- *
- * @author Ravikumar
- */
-import java.awt.event.*; 
-import java.applet.*; 
 import java.awt.*;
-
-public class TrafficLight extends Applet implements ItemListener  {
-String msg = "";
-Checkbox red,green,yellow; 
-CheckboxGroup cbg;
-public void init() {
-	cbg = new CheckboxGroup();
-	red = new Checkbox("Red", cbg, false); 
-	green = new Checkbox("Green", cbg, false);
-	yellow = new Checkbox("Yellow", cbg, false); 
-	add(red);
-	add(yellow);
-	add(green);
-	red.addItemListener(this);
-	yellow.addItemListener(this);
-	green.addItemListener(this);
-}
-public void itemStateChanged(ItemEvent ie) 
-{ 
-	repaint();
-}
-public void paint(Graphics g) 
+import java.applet.*;
+import java.awt.event.*;
+/*<applet code = TrafficSignal width = 500 height = 500></applet>*/
+public class TrafficSignal extends Applet implements ItemListener
 {
-	Color color;
-	color=Color.BLACK;
-	g.setColor(color); 
-	g.drawOval(50, 50, 52, 52); 
-	g.drawOval(50, 103, 52, 52);
-	g.drawOval(50, 156, 52, 52);
-	String col = cbg.getSelectedCheckbox().getLabel();
-	System.out.println(col);
-if(col.equalsIgnoreCase("Green"))
-{
-	color= Color.GREEN; 
-	g.setColor(color); 
-	g.fillOval(50, 156, 52, 52); 
-	g.drawString("GO",110,190);
-}
-if(col.equalsIgnoreCase("Red"))
-{
-	color=Color.RED;
-	g.setColor(color); 
-	g.fillOval(51, 51, 51, 51); 
-	g.drawString("STOP",110,80);
-}
-if(col.equalsIgnoreCase("Yellow"))
-{
-	color=Color.YELLOW;
-	g.setColor(color);
-	g.fillOval(50, 103, 51, 51);
-	color= Color.BLACK; 
-	g.setColor(color);
-	g.drawString("READY",110,140);
-}
-}
-}
-
-
+	String msg = " ";
+	Checkbox red,green,yellow;
+	CheckboxGroup grp;
+	public void init()
+	{
+		grp = new CheckboxGroup();
+		red = new Checkbox("Red",grp,false);
+		green = new Checkbox("Green",grp,false);
+		yellow = new Checkbox("Yellow",grp,false);
+		add(red);
+		add(green);
+		add(yellow);
+		red.addItemListener(this);
+		green.addItemListener(this);
+		yellow.addItemListener(this);
+	}
+	public void itemStateChanged(ItemEvent e)
+	{
+		repaint();
+	}
+	public void paint(Graphics g)
+	{
+		msg = grp.getSelectedCheckbox().getLabel();
+		setBackground(Color.white);
+		g.drawOval(50, 50, 52, 52); 
+		g.drawOval(50, 103, 52, 52);
+		g.drawOval(50, 156, 52, 52);
+		if(msg.equalsIgnoreCase("Green"))
+		{
+			g.setColor(Color.green);
+			g.fillOval(50, 156, 52, 52); 
+			g.drawString("Go",110,190);
+		}
+		else if(msg.equalsIgnoreCase("yellow"))
+		{
+			g.setColor(Color.yellow);
+			g.fillOval(50, 103, 52, 52); 
+			g.drawString("Ready",110,135);
+		}
+		else if(msg.equalsIgnoreCase("red"))
+		{
+			g.setColor(Color.red);
+			g.fillOval(50, 50, 52, 52); 
+			g.drawString("Stop!",110,85);
+		}
+	}
+	
